d96546011d6403f67ae718e584c3db2d78565250
kellyss1 <kdledesma@uce.edu.ec>
2025-11-21 19:43:05 -0500

    Taller 13

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hola Mundo Desde Vue - Ledesma"/>
+  <div>
+    <ContadorView></ContadorView>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script>  //logica de javascript
+import ContadorView from '../src/views/ContadorView.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  componenets: {
+    ContadorView
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style> /* estilos CSS */
+
 </style>
---
+++ src/views/ContadorView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Universidad Central del Ecuador</h1>
+    <h2>Hola Mundo</h2>
+  </div>
+</template>
+
+<script>
+import Contador from '../components/Contador.vue'
+export default {
+
+    components: {
+        Contador,
+    },
+}
+</script>
+
+<style>
+
+</style>
