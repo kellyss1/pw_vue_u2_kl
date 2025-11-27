0d06eb94546ca7f64d76e727b3ce80d13c4aae15
kellyss1 <kdledesma@uce.edu.ec>
2025-11-27 18:16:48 -0500

    Taller 14 - Directivas Vue

---
+++ src/components/Contador.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1>
+    <h1>{{ tit }}</h1> 
+    <p>{{ numero }}<sup>2</sup>= {{ calculaCuadrado }}</p>
+    <p>{{ numero }}<sup>2</sup>= {{ calculaCuadrado }}</p>
+    <p>{{ numero }}<sup>3</sup>= {{ obtenerCubo() }}</p>
+    <p>{{ numero }}<sup>2</sup>= {{ calculaCuadrado }}</p>
+    <p>{{ numero }}<sup>2</sup>= {{ calculaCubo }}</p>
+    <div>
+      <button v-on:click="incrementar()">+1</button>
+      <button v-on:click="decrementar()">-1</button>
+      <button v-show="mostrar">prueba</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() { //propiedades reactivas
+    return {
+      numero: this.inicio,
+      titulo: 'Contador'
+    }
+  }, 
+  methods:{
+    obtenerCuadrado() {
+      console.log('Entró a obtener el cuadrado');
+      return this.numero * this.numero;
+    },
+    obtenerCubo() {
+      console.log('Entró a obtener cubo');
+      return this.numero * this.numero * this.numero;    
+    },
+    incrementar() {
+      console.log(this.tit);
+      this.numero ++;
+    },
+    decrementar() {
+      this.numero --;
+    }
+  },
+  computed: {
+    calculaCuadrado() {
+      console.log('Entró a propiedad computada');
+      return this.numero * this.numero;
+    },
+    calculaCubo() {
+      console.log('Entró a una propiedad computada');
+      return this.numero * this.numero * this.numero;
+    },
+  },
+  //props: ['tit', 'inicio', 'mostrar'], Declaracion básica
+  props:{
+    tit: String,
+    inicio: {
+      type: Number,
+      default: 99,
+      validator(value) {
+        return value > 10;
+      }
+    },
+    mostrar: Boolean
+  },
+  // props: pueden ser tipo string, boolean, array, objetct, date, funiction, BigInt
+};
+</script>
+
+<style>
+
+</style>
--- src/views/ContadorView.vue
+++ src/views/ContadorView.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <h1>Universidad Central del Ecuador</h1>
-    <h2>Hola Mundo</h2>
+       <Contador tit="Contador 1" v-bind:inicio="4+4" :mostrar="true"/>
+       <Contador tit="Contador 2" :inicio="10" :mostrar="false"/>
   </div>
 </template>
 
 <script>
-import Contador from '../components/Contador.vue'
+import Contador from '../components/Contador.vue';
+
 export default {
 
     components: {
         Contador,
     },
-}
+};
 </script>
 
 <style>
