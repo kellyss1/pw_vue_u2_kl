e975de1c2a857c9a680beca2eab6e2db2e2cf95c
kellyss1 <kdledesma@uce.edu.ec>
2025-12-11 22:07:29 -0500

    Refactor Pregunta.vue: streamline image rendering and enhance API call logic

--- src/components/Pregunta.vue
+++ src/components/Pregunta.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <img
-      v-if="imagen"
-      :src="imagen"
-      alt="No se puede cargar la imagen"
-    />
+    <img v-if="imagen" :src="imagen" alt="No se puede cargar la imagen" />
     <div class="oscuro">
       <div class="pregunta-container">
         <input
@@ -24,35 +20,37 @@
 </template>
 
 <script>
-import { consumirAPIFacade } from "../clients/YesNoClient.js";
-
+// : enlace reactivo (v-bind)
+import { consumirAPIFacade } from "../clients/YesNoClient.js"; // Entre llaves importar la función específica
 export default {
   data() {
+    // propiedad reactiva
     return {
       pregunta: null,
       respuesta: null,
       imagen: null,
     };
   },
-
   watch: {
+    // observar cambios en las propiedades reactivas, se disparan cuando estas cambian
     pregunta(value, oldValue) {
-      //sync la propiedad reactiva con el input (ejemplo)
       //console.log(value);
       //console.log(oldValue);
 
       if (value.includes("?")) {
+        // Llamar a la API solo si la pregunta tiene signo de interrogación
+        console.log("Es una pregunta");
+        this.respuesta = "...cargando"; // Mensaje de carga mientras se espera la respuesta
         this.consumir();
       }
     },
   },
   methods: {
     async consumir() {
-      //llamar al API
       const resp = await consumirAPIFacade();
-      console.log("Respuesta Final");
+      console.log("Respuesta final");
       console.log(resp);
-      console.log(resp.answer);
+      console.log(resp.answer); // Esperar una respuesta de la API, axios no lo hace por defecto
       this.respuesta = resp.answer;
       this.imagen = resp.image;
     },
@@ -61,7 +59,8 @@ export default {
 </script>
 
 <style>
-.oscuro, img {
+img,
+.oscuro {
   height: 100vh;
   width: 100vw;
   max-height: 100%;
@@ -69,6 +68,11 @@ export default {
   position: fixed;
   left: 0px;
   top: 0px;
+}
+
+.oscuro {
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 1;
 }
 
 .pregunta-container {
@@ -109,10 +113,5 @@ h2 {
   margin-top: 40px;
   font-size: 30px;
   color: #42b983;
-}
-
-.oscuro {
-  background-color: gray;
-  z-index: 1;
 }
 </style>
